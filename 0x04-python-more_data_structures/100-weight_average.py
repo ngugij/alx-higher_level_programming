a286a1baa4e43d97afb8eaeaa4b0bb1662867e05
ngugij <ngugijse22@gmail.com>
2022-09-28 19:01:39 -0700

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        score = 0
+        weight = 0
+        for pair in my_list:
+            score = score + (pair[0] * pair[1])
+            weight = weight + pair[1]
+        return (score / weight)
+    return (0)
