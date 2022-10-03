b58fd942cbb98148dc6d718d7bfe0fa48d307f7c
ngugij <ngugijse22@gmail.com>
2022-10-03 09:57:58 -0700

    Biggest integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for elem in my_list:
+            if elem > max:
+                max = elem
+        return max
+    return (None)
