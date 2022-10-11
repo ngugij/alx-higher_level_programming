583962d7a45b62cbc5fe032e30f3acb23cd6cf4d
ngugij <ngugijse22@gmail.com>
2022-10-11 14:31:59 -0700

    Print integer and print error in stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        import stderr from sys
+        from sys import stderr
         print("Exception: {}".format(err), file=stderr)
         return (False)
     else:
