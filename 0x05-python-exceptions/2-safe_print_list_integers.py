78f2bd35effcfbca85562b1f3c4e05c27149783f
ngugij <ngugijse22@gmail.com>
2022-10-11 14:00:31 -0700

    Print x elements of a list only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     a = 0
     for i in range(x):
         try:
-            print("{:d}".format(mylist[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             a = a + 1
         except (ValueError, TypeError):
             continue
