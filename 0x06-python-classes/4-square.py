6a906aff965dd8a6adaa6c4e7071df2caa773cfc
ngugij <ngugijse22@gmail.com>
2022-10-12 15:23:14 -0700

    Property and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class defining a square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Initializes square
+        Args:
+            size (int): size of a square
+        """
+
+    @property
+    def size(self):
+        """Retrieves size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return area of a square"""
+        return (self.__size ** 2)
