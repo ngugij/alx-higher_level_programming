146d1869299564a957ec247c616acf451ca4dd32
ngugij <ngugijse22@gmail.com>
2022-10-12 14:50:40 -0700

    Square private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class defining a square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
