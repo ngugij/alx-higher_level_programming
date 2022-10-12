1b32f4331d467c18c2e9a7a3aa2db92afbf20e30
ngugij <ngugijse22@gmail.com>
2022-10-12 15:09:19 -0700

    Return area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class defining a square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Initializes square
+        Args:
+            size (int): size of a square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return area of a square"""
+        return (self.__size ** 2)
