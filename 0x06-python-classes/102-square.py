93feaddfc2bc42d9f179ec6b8fc1dfcedc89dc05
ngugij <ngugijse22@gmail.com>
2022-10-12 19:56:10 -0700

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Class defining a square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Initializes square
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
+
+    def area(self):
+        """Returns area of a square"""
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """Retrieves size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
