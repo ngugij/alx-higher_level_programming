052fb38d5ed77e4064ec1da87d0da0a9437e02da
ngugij <ngugijse22@gmail.com>
2022-10-17 19:30:49 -0700

    Rectangle is a square?

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -66,7 +66,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Returns a new Rectangle instance with width == height == size"""
-        return cls(size, size)
+        return (cls(size, size))
 
     def __str__(self):
         """prints the rectangle with character #"""
