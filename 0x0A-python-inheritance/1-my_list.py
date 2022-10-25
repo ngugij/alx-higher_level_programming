cab8e1c8c51020076fd9b1b3ed1128a0dbd4a0be
ngugij <ngugijse22@gmail.com>
2022-10-24 17:51:39 -0700

    Print sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@ inherits from list"""
 
 class MyList(list):
     """list is inherited by MyList"""
+    def __init__(self):
+        super().__init__()
+
     def print_sorted(self):
         """prints list in ascending order"""
         print(sorted(self))
