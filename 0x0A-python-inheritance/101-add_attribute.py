02cd3a33f524c509867cc657467b9d1949df33f4
ngugij <ngugijse22@gmail.com>
2022-10-24 19:58:46 -0700

    Add new attribute to object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,11 +2,10 @@
 """add_attribute module"""
 
 
-class add_attribute(obj, atr, value):
+def add_attribute(obj, atr, value):
     """adds a new attribute to an object
         if it’s possible"""
-    check = {int, str, float, list, dict, tuple, frozenset, type, object}
 
-    if type(obj) in check:
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    obj.__setattr__(atr, value)
+    setattr(obj, atr, value)
