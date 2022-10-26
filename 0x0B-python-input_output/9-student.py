261e5a6ccd1dd500a2e5073ef72a7133ea3b1161
ngugij <ngugijse22@gmail.com>
2022-10-25 18:24:55 -0700

    Student dictionary

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,11 @@ class Student:
         description of student"""
 
     def __init__(self, first_name, last_name, age):
+        """initialize student"""
         self.first_name = first_name
         self.lst_name = last_name
         self.age = age
 
     def to_json(self):
+        """returns dictionary representation"""
         return self.__dict__
